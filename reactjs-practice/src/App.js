c61bf35695df47012c6d8a47cd20aa86f71692d7
Manu <manuel.chavez@accenture.com>
2022-08-10 14:43:19 -0500

    Use state is better to manage form elements

--- reactjs-practice/src/App.js
+++ reactjs-practice/src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
-import  { useRef } from 'react';
+import  { useState } from 'react';
 
 function App() {
-  const txtTitle = useRef();
-  const hexColor = useRef();
-
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState("#000000");
 const submit = (e) => {
   e.preventDefault();
-  const title = txtTitle.current.value;
-  const color = hexColor.current.value;
   alert(`${title}, ${color}`);
-  txtTitle.current.value = "";
-  hexColor.current.value = "";
+  setTitle("");
+  setColor("#000000");
 };
   return (
    <form onSubmit={submit}>
     <input
-      ref={txtTitle} type="text" placeholder='color title...'
+      value={title} 
+      onChange={(event) => setTitle(event.target.value)} 
+      type="text" 
+      placeholder='color title...'
     />
     <input
-      ref={hexColor} type="color"
+      value={color}
+      onChange={(event) => setColor(event.target.value)}
+      type="color"
     />
     <button>Add</button>
    </form>

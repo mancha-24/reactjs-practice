0707e517bbb00110d307da8966b94635d5b57241
Manu <manuel.chavez@accenture.com>
2022-08-10 13:41:25 -0500

    understanding useEffect on React

--- reactjs-practice/src/App.js
+++ reactjs-practice/src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const what = useState();
+
   const [emotion, setEmotion] = useState("happy"); 
+  const [secundary, setSecundary] = useState("tired"); 
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secundary} right now`);
+  }, []);
+
+  const what = useState();
+  
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>    
       <button onClick={() => setEmotion("sad")}>Sad</button>
       <button onClick={() => setEmotion("excited")}>Excited</button>
+
+      <h2>Current secundary emotion is {secundary}</h2>   
+      <button onClick={() => setSecundary("gratefull")}>Grateful</button>
     </div>
   );
 }

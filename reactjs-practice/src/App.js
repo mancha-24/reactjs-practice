74b6798bdd096079192bbd0c80ab6aa43adaad9c
Manu <manuel.chavez@accenture.com>
2022-08-10 13:30:24 -0500

    understanding useState in react

--- reactjs-practice/src/App.js
+++ reactjs-practice/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const what = useState();
+  const [emotion, setEmotion] = useState("happy"); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Current emotion is {emotion}</h1>    
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
     </div>
   );
 }
